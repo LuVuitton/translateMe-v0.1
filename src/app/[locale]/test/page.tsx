5296761542d5d212445786cbdfef77690b3af507
FelixT <felixtekhada@gmail.com>
2023-12-15 18:03:13 +0100

    removed test page content

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -1,18 +1,9 @@
-"use client";
-
-import s from "./test.module.scss";
-
-
-const elements = [1,2,3,4,5,6,7]
-
 export default function Test() {
 
-const mapped = elements.map(e=> <div className={s.el}>hello</div> )
 
   return (
-    <div className={s.main}>
-      <span className={`${s.item} ${s.absolute}`}>{mapped}</span>
-      <span className={`${s.item} ${s.fixed}`}>fixed</span>
+    <div >
+      test
     </div>
   );
 }
