36e258435bf039e58cda0cdf5bed515a54cc21f4
FelixT <felixtekhada@gmail.com>
2023-12-16 00:07:24 +0100

    turned on sign-in

--- src/app/[locale]/sign-in/page.tsx
+++ src/app/[locale]/sign-in/page.tsx
@@ -3,6 +3,6 @@ import { SignIn } from "@/components/modules";
 
 
 export default function SignInPage() {
-  // return <SignIn />;
-  return <div>SignIn </div>;
+  return <SignIn />;
+  // return <div>SignIn </div>;
 }
