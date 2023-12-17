830c240f9aa0a08574fe28ab7dbbd4977f955875
FelixT <felixtekhada@gmail.com>
2023-12-17 08:43:47 +0100

    fixed export file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,16 @@
-export { default as TheButton } from "./theButton";
+export { default as TheButton } from "./theButton/index";
 // export { default as Candidates } from "./candidates";
-export { default as Candidates } from "./candidates";
-export { default as Preloader } from "./preloader";
-export { default as Section } from "./section";
-export { default as Title } from "./title";
-export { default as NoAssignmnents } from "./noAssignmnents";
+export { default as Candidates } from "./candidates/index";
+export { default as Preloader } from "./preloader/index";
+export { default as Section } from "./section/index";
+export { default as Title } from "./title/index";
+export { default as NoAssignmnents } from "./noAssignmnents/index";
 
-export { default as DatePicker } from "./form/datePicker";
-export { default as FormInput } from "./form/formInput";
-export { default as FormNumberController } from "./form/formNumberController";
-export { default as FormSelectController } from "./form/formSelectController ";
-export { default as InputError } from "./form/inputError";
-export { default as TheSelect } from "./form/select";
-export { default as TheInputNumber } from "./form/theNumber";
+export { default as DatePicker } from "./form/datePicker/index";
+export { default as FormInput } from "./form/formInput/index";
+export { default as FormNumberController } from "./form/formNumberController/index";
+export { default as FormSelectController } from "./form/formSelectController /index";
+export { default as InputError } from "./form/inputError/index";
+export { default as TheSelect } from "./form/select/index";
+export { default as TheInputNumber } from "./form/theNumber/index";
 
